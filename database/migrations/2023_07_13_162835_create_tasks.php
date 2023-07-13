2b1897e679be6a09f5f1b4d7da036cee4ded8968
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-07-13 14:02:10 -0300

    feita migration e model de tarefas e atualizado model de user

---
+++ database/migrations/2023_07_13_162835_create_tasks.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->date('due_in');
+            $table->boolean('completed');
+            $table->date('completion_date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
